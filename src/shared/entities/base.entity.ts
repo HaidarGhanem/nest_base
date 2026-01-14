b3d61d4b08db7fa107b0300a75c1bfe5c56813b2
HaidarGhanem <haidar23ghanem@gmail.com>
2026-01-14 22:59:37 +0300

    base entity

---
+++ src/shared/entities/base.entity.ts
@@ -0,0 +1,15 @@
+import { Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, DeleteDateColumn } from "typeorm";
+
+export abstract class BaseEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string 
+
+    @CreateDateColumn()
+    createdAt: Date
+
+    @UpdateDateColumn()
+    updatedAt: Date 
+
+    @DeleteDateColumn()
+    deletedAt: Date
+}
