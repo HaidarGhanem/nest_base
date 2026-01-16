fa831e647c98ebe86b2dc56f5a8120f0aba8b4cf
HaidarGhanem <haidar23ghanem@gmail.com>
2026-01-16 14:51:03 +0300

    user's data decorator

---
+++ src/common/decorators/user.decorator.ts
@@ -0,0 +1,10 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { IUser } from '../interfaces/user.interface';
+
+export const User = createParamDecorator(
+  (data: keyof IUser, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    const user = request.user as IUser;
+    return data ? user?.[data] : user;
+  },
+);
